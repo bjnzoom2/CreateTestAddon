90d9b62dfba5811d51d7dcfc4a18f86219cf3a17
bjnzoom2 <bjnzoom2@gmail.com>
2025-09-01 17:10:09 +0800

    Commit

--- src/main/java/net/bjnzoom2/createtestaddon/registry/CTABlocks.java
+++ src/main/java/net/bjnzoom2/createtestaddon/registry/CTABlocks.java
@@ -4,37 +4,28 @@ import com.simibubi.create.AllTags;
 import com.simibubi.create.api.stress.BlockStressValues;
 import com.simibubi.create.foundation.block.ItemUseOverrides;
 import com.simibubi.create.foundation.data.BlockStateGen;
-import com.simibubi.create.foundation.data.CreateRegistrate;
 import com.simibubi.create.foundation.data.SharedProperties;
-import com.simibubi.create.content.kinetics.simpleRelays.BracketedKineticBlockModel;
 import com.tterrag.registrate.util.entry.BlockEntry;
 import net.bjnzoom2.createtestaddon.block.kinetic.crank_wheel.CrankWheelBlock;
-import net.bjnzoom2.createtestaddon.block.kinetic.custom_cogs.CustomCogwheelBlock;
-import net.bjnzoom2.createtestaddon.block.kinetic.custom_cogs.CustomCogwheelBlockItem;
 import net.bjnzoom2.createtestaddon.config.CTAStress;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.material.MapColor;
 
-import java.util.Objects;
-
 import static com.simibubi.create.foundation.data.ModelGen.customItemModel;
-import static com.simibubi.create.foundation.data.TagGen.axeOnly;
-import static com.simibubi.create.foundation.data.TagGen.axeOrPickaxe;
+import static com.simibubi.create.foundation.data.TagGen.*;
 import static net.bjnzoom2.createtestaddon.CreateTestAddon.REGISTRATE;
 
 public class CTABlocks {
     public static BlockEntry<Block> RAW_BRASS_BLOCK = REGISTRATE.block("raw_brass_block", Block::new)
             .lang("Block of Raw Brass")
+            .properties(p -> p.mapColor(MapColor.TERRACOTTA_YELLOW))
             .simpleItem()
             .register();
 
-    public static final BlockEntry<CustomCogwheelBlock> IRON_COGWHEEL = createCustomCogwheel("iron");
-    public static final BlockEntry<CustomCogwheelBlock> IRON_LARGE_COGWHEEL = createLargeCustomCogwheel("iron");
-
     public static final BlockEntry<CrankWheelBlock.Small> CRANK_WHEEL = REGISTRATE.block("crank_wheel", CrankWheelBlock.Small::new)
             .initialProperties(SharedProperties::wooden)
-            .properties(p -> p.mapColor(MapColor.PODZOL))
+            .properties(p -> p.sound(SoundType.WOOD).mapColor(MapColor.PODZOL))
             .transform(axeOrPickaxe())
             .blockstate(BlockStateGen.directionalBlockProvider(true))
             .transform(CTAStress.setCapacity(8.0))
@@ -47,7 +38,7 @@ public class CTABlocks {
 
     public static final BlockEntry<CrankWheelBlock.Large> LARGE_CRANK_WHEEL = REGISTRATE.block("large_crank_wheel", CrankWheelBlock.Large::new)
             .initialProperties(SharedProperties::wooden)
-            .properties(p -> p.mapColor(MapColor.PODZOL))
+            .properties(p -> p.sound(SoundType.WOOD).mapColor(MapColor.PODZOL))
             .transform(axeOrPickaxe())
             .blockstate(BlockStateGen.directionalBlockProvider(true))
             .transform(CTAStress.setCapacity(8.0))
@@ -57,34 +48,6 @@ public class CTABlocks {
             .item()
             .transform(customItemModel())
             .register();
-
-    private static BlockEntry<CustomCogwheelBlock> createCustomCogwheel(String name) {
-        return REGISTRATE.block(name + "_cogwheel", CustomCogwheelBlock::small)
-                .initialProperties(SharedProperties::stone)
-                .properties(p -> p.sound(SoundType.WOOD).mapColor(MapColor.DIRT))
-                .transform(CTAStress.setNoImpact())
-                .transform(axeOnly())
-                .blockstate(CTABlockStateGens.cogwheel(name))
-                .onRegister(CreateRegistrate.blockModel(() -> BracketedKineticBlockModel::new))
-                .item(CustomCogwheelBlockItem::new)
-                .model((c,p)->p.getBuilder(c.getName()).parent(Objects.requireNonNull(cogwheelModel(p, name,true))))
-                .build()
-                .register();
-    }
-
-    private static BlockEntry<CustomCogwheelBlock> createLargeCustomCogwheel(String name) {
-        return REGISTRATE.block(name + "_large_cogwheel", CustomCogwheelBlock::large)
-                .initialProperties(SharedProperties::stone)
-                .properties(p -> p.sound(SoundType.WOOD).mapColor(MapColor.DIRT))
-                .transform(axeOnly())
-                .transform(CTAStress.setNoImpact())
-                .blockstate(CTABlockStateGens.largeCogwheel(name))
-                .onRegister(CreateRegistrate.blockModel(() -> BracketedKineticBlockModel::new))
-                .item(CustomCogwheelBlockItem::new)
-                .model((c,p)->p.getBuilder(c.getName()).parent(Objects.requireNonNull(largeCogwheelModel(p, name,true))))
-                .build()
-                .register();
-    }
 
     public static void register() {}
 }
--- src/main/java/net/bjnzoom2/createtestaddon/registry/CTAPartialModels.java
+++ src/main/java/net/bjnzoom2/createtestaddon/registry/CTAPartialModels.java
@@ -12,20 +12,6 @@ public class CTAPartialModels {
     public static final PartialModel LARGE_CRANK_WHEEL_HANDLE = block("large_crank_wheel/handle");
     public static final PartialModel LARGE_CRANK_WHEEL_BASE = block("large_crank_wheel/block");
 
-    public static final Map<String, PartialModel> COGS_MODELS = new HashMap<>();
-    public static final Map<String, PartialModel> LARGE_COGS_MODELS = new HashMap<>();
-    public static final Map<String, PartialModel> SHAFTLESS_COGS_MODELS = new HashMap<>();
-    public static final Map<String, PartialModel> SHAFTLESS_LARGE_COGS_MODELS = new HashMap<>();
-    static {
-        String[] materials = new String[]{"iron"};
-        for (String m : materials) {
-            COGS_MODELS.put(m, CTAPartialModels.block("cogwheel/" + m));
-            LARGE_COGS_MODELS.put(m, CTAPartialModels.block("large_cogwheel/" + m));
-            SHAFTLESS_COGS_MODELS.put(m, CTAPartialModels.block("cogwheel_shaftless/" + m));
-            SHAFTLESS_LARGE_COGS_MODELS.put(m, CTAPartialModels.block("large_cogwheel_shaftless/" + m));
-        }
-    }
-
     private static PartialModel block(String path) {
         return PartialModel.of(CreateTestAddon.asResource("block/" + path));
     }

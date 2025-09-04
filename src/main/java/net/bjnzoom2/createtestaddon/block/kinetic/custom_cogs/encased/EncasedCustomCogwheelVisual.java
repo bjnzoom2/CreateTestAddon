b3238da37f4869f7d566f971286b59eadd5c6f86
bjnzoom2 <bjnzoom2@gmail.com>
2025-09-04 16:45:47 +0800

    Commit

--- src/main/java/net/bjnzoom2/createtestaddon/block/kinetic/custom_cogs/encased/EncasedCustomCogwheelVisual.java
+++
@@ -1,4 +0,0 @@
-package net.bjnzoom2.createtestaddon.block.kinetic.custom_cogs.encased;
-
-public class EncasedCustomCogwheelVisual {
-}
--- src/main/java/net/bjnzoom2/createtestaddon/registry/CTABlockEntityTypes.java
+++ src/main/java/net/bjnzoom2/createtestaddon/registry/CTABlockEntityTypes.java
@@ -1,12 +1,9 @@
 package net.bjnzoom2.createtestaddon.registry;
 
-import com.simibubi.create.AllBlocks;
 import com.simibubi.create.content.kinetics.crank.HandCrankRenderer;
-import com.simibubi.create.content.kinetics.simpleRelays.SimpleKineticBlockEntity;
 import com.tterrag.registrate.util.entry.BlockEntityEntry;
 import net.bjnzoom2.createtestaddon.block.kinetic.crank_wheel.CrankWheelBlockEntity;
 import net.bjnzoom2.createtestaddon.block.kinetic.crank_wheel.CrankWheelVisual;
-import net.bjnzoom2.createtestaddon.block.kinetic.custom_cogs.encased.EncasedCustomCogwheelVisual;
 
 import static net.bjnzoom2.createtestaddon.CreateTestAddon.REGISTRATE;
 
